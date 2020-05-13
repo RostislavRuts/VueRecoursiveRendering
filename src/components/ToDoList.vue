cb196cde2176ffe967207c8718b5f631e3d1fb92
RostislavRuts <57718613+RostislavRuts@users.noreply.github.com>
2020-05-14 02:04:26 +0300

    control

--- src/components/ToDoList.vue
+++ src/components/ToDoList.vue
@@ -1,47 +1,44 @@
 <template>
-    <ul class="todolist">
-        <ToDoItem
-                :item="item"
-                :index="index"
-                v-for="(item, index) in items"
-                :allitems="items"
-                :key="item.id"
-                @deleteTask="deleteTask"
-                @item-up="moveItemUp"
-       />
+  <ul class="todolist">
+    <ToDoItem
+      :item="item"
+      :index="index"
+      v-for="(item, index) in items"
+      :allitems="items"
+      :key="item.id"
+      @deleteTask="deleteTask"
+      @item-up="moveItemUp"
+    />
 
-        <AddTask
-                @add-main-list="addTask"
-                :task="task"
-        />
-    </ul>
+    <AddTask @add-main-list="addTask" :task="task"/>
+  </ul>
 </template>
 
 <script>
-    import ToDoItem from '@/components/ToDoItem';
-    import AddTask from '@/components/AddTask';
-    export default {
-        components: {
-            ToDoItem,
-            AddTask
-        },
-        data() {
-            return {
-                items: [],
-                task: 'Task'
-            }
-        },
-        methods: {
-            addTask(task) {
-                this.items.push(task)
-                //console.log(this.items)
-            },
-            deleteTask(index) {
-                this.items.splice(index, 1)
-            },
-            moveItemUp(el){
-                console.log(el)
-            }
-        }
+import ToDoItem from "@/components/ToDoItem";
+import AddTask from "@/components/AddTask";
+export default {
+  components: {
+    ToDoItem,
+    AddTask
+  },
+  data() {
+    return {
+      items: [],
+      task: "Task"
+    };
+  },
+  methods: {
+    addTask(task) {
+      this.items.push(task);
+      //console.log(this.items)
+    },
+    deleteTask(index) {
+      this.items.splice(index, 1);
+    },
+    moveItemUp(el) {
+      console.log(el)
     }
+  }
+};
 </script>
